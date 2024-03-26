ade75efaf90c049ffa9fc41625d31cea2233ae59
woogym <kwj9294@gmail.com>
2024-03-27 01:44:31 +0900

    modify main method

--- serverbuild1200/src/git_test2/Main.java
+++ serverbuild1200/src/git_test2/Main.java
@@ -2,6 +2,8 @@ package git_test2;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("안녕하세요");
+
+        Hello h = new Hello();
+        System.out.println(h.message);
     }
 }
